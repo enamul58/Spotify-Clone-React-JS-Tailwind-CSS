25538cd101fc98b40985586a97924fc35145a80b
“enamul58” <enamullimon@gmail.com>
2024-07-02 23:13:55 +0600

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/{Spotify-Clone-React-JS-Tailwind-CSS}",
   plugins: [react()],
+  base: "/Spotify-Clone-React-JS-Tailwind-CSS/",
 });
